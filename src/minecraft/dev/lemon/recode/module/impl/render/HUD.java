02a2c1e0fe4cdca21155618b8ab71b16da52c27b
Eternadox <77886012+eternadox@users.noreply.github.com>
2023-07-03 15:50:24 -0400

    added color fade to hud
    yeah

--- src/minecraft/dev/lemon/recode/module/impl/render/HUD.java
+++ src/minecraft/dev/lemon/recode/module/impl/render/HUD.java
@@ -6,19 +6,21 @@ import dev.lemon.recode.event.impl.Event2DRender;
 import dev.lemon.recode.module.Category;
 import dev.lemon.recode.module.Module;
 import dev.lemon.recode.module.ModuleInfo;
+import dev.lemon.recode.utils.render.ColorUtil;
 import net.minecraft.util.EnumChatFormatting;
 import org.lwjgl.input.Keyboard;
 
 @ModuleInfo(name = "HUD", key = Keyboard.KEY_O, category = Category.RENDER, suffix = "")
 public class HUD extends Module {
-
+    int color = ColorUtil.fadeBetween(0xffffff00, 0xff00ffff, (float) ((System.currentTimeMillis()) % 1000L) / 500.0f);
     @EventHandler
     public Listener<Event2DRender> event2DRenderListener = e -> {
-        mc.fontRendererObj.drawStringWithShadow(lemon.getName().substring(0, 1)+ EnumChatFormatting.WHITE+lemon.getName().substring(1), 5, 5, 0xFFFFFF00);
+        mc.fontRendererObj.drawStringWithShadow(lemon.getName().substring(0, 1)+ EnumChatFormatting.WHITE+lemon.getName().substring(1), 5, 5, color);
         int moduleY = 4;
         int spacing = 2;
         int index = 0;
         for (Module m : lemon.getModuleManager().getEnabledSortedModules()){
+            color = ColorUtil.fadeBetween(0xffffff00, 0xff00ffff, (float) ((System.currentTimeMillis() + index * 100L) % 1000L) / 500.0f);
             mc.fontRendererObj.drawStringWithShadow(m.getName()+(m.getSuffix().isEmpty() ? "" : " ")+EnumChatFormatting.WHITE+m.getSuffix(),e.getWidth() - mc.fontRendererObj.getStringWidth(m.getName()+(m.getSuffix().isEmpty() ? "" : " ")+EnumChatFormatting.WHITE+m.getSuffix()) - 5, moduleY, -1);
             moduleY += mc.fontRendererObj.FONT_HEIGHT+spacing;
             index++;
