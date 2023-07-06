4476d0ce491735324cf6e9c15ad79a8de40ea1d1
Eternadox <77886012+eternadox@users.noreply.github.com>
2023-07-06 18:55:20 -0400

    added fading colors to moveutil

--- src/minecraft/dev/lemon/recode/module/impl/render/HUD.java
+++ src/minecraft/dev/lemon/recode/module/impl/render/HUD.java
@@ -17,13 +17,13 @@ public class HUD extends Module {
     @EventHandler
     public Listener<Event2DRender> event2DRenderListener = e -> {
         ScaledResolution sr = new ScaledResolution(mc);
-        int color = ColorUtil.lemonColors2(1.5F, 6 * -10);
+        int color = ColorUtil.fadeLemonColors(0);
         mc.fontRendererObj.drawStringWithShadow(lemon.getName().substring(0, 1) + EnumChatFormatting.WHITE + lemon.getName().substring(1), 5, 5, color);
         int offsetY = 4;
         int spacing = 2;
         int index = 0;
         for (Module m : lemon.getModuleManager().getEnabledSortedModules()) {
-            color = ColorUtil.lemonColors2(1.5F, 6 * -10);
+            color = ColorUtil.fadeLemonColors(index);
             mc.fontRendererObj.drawStringWithShadow(m.getName() + (m.getSuffix().isEmpty() ? "" : " ") + EnumChatFormatting.WHITE + m.getSuffix(), e.getWidth() - mc.fontRendererObj.getStringWidth(m.getName() + (m.getSuffix().isEmpty() ? "" : " ") + EnumChatFormatting.WHITE + m.getSuffix()) - 5, offsetY, color);
             offsetY += mc.fontRendererObj.FONT_HEIGHT + spacing;
             index++;
--- src/minecraft/dev/lemon/recode/utils/render/ColorUtil.java
+++ src/minecraft/dev/lemon/recode/utils/render/ColorUtil.java
@@ -47,6 +47,27 @@ public class ColorUtil {
         return getGradientOffset(new Color(0, 255, 13), new Color(255, 234, 1), hue2).getRGB();
     }
 
+    public static int fadeColors(int color1, int color2, float time ){
+            if (time > 1.0F)
+                time = 1.0F - time % 1.0F;
+            double d = (1.0F - time);
+            int i = (int)((color1 >> 16 & 0xFF) * d + ((color2 >> 16 & 0xFF) * time));
+            int j = (int)((color1 >> 8 & 0xFF) * d + ((color2 >> 8 & 0xFF) * time));
+            int k = (int)((color1 & 0xFF) * d + ((color2 & 0xFF) * time));
+            int m = (int)((color1 >> 24 & 0xFF) * d + ((color2 >> 24 & 0xFF) * time));
+            return (m & 0xFF) << 24 | (i & 0xFF) << 16 | (j & 0xFF) << 8 | k & 0xFF;
+
+    }
+
+    public static int fadeLemonColors(int index){
+       return fadeColors(0xFF00ff0d, 0xFFffea01, index);
+
+    }
+
+    public static int fadeColors(int color1, int color2, int index){
+        return fadeColors(color1, color2, (float) ((System.currentTimeMillis() + index * 100L) % 1000L) / 500.0f);
+
+    }
 
     public static int lemonGreenColors(float seconds, long index) {
         index += 40;
