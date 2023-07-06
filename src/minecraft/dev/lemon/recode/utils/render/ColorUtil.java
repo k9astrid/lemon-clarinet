5c761e8223ecda9b46667fed61c2e943c7573620
ClpzW <104445386+ClpzW@users.noreply.github.com>
2023-07-06 18:07:39 +0300

    recoded colorutil so its actually usable, added that chat thing to the binds

--- src/minecraft/dev/lemon/recode/utils/render/ColorUtil.java
+++ src/minecraft/dev/lemon/recode/utils/render/ColorUtil.java
@@ -1,19 +1,82 @@
 package dev.lemon.recode.utils.render;
 
+import java.awt.Color;
 
-import dev.lemon.recode.utils.Util;
+public class ColorUtil {
 
-public class ColorUtil implements Util {
+    public static int getRGB(float seconds, float saturation, float brightness, long index) {
+        float hue = ((System.currentTimeMillis() + index) % (int)(seconds * 1000)) / (float)(seconds * 1000);
+        int color = Color.HSBtoRGB(hue, saturation, brightness);
+        return color;
+    }
 
-    public static int fadeBetween(int paramInt1, int paramInt2, float paramFloat) {
-        if (paramFloat > 1.0F)
-            paramFloat = 1.0F - paramFloat % 1.0F;
-        double d = (1.0F - paramFloat);
-        int i = (int)((paramInt1 >> 16 & 0xFF) * d + ((paramInt2 >> 16 & 0xFF) * paramFloat));
-        int j = (int)((paramInt1 >> 8 & 0xFF) * d + ((paramInt2 >> 8 & 0xFF) * paramFloat));
-        int k = (int)((paramInt1 & 0xFF) * d + ((paramInt2 & 0xFF) * paramFloat));
-        int m = (int)((paramInt1 >> 24 & 0xFF) * d + ((paramInt2 >> 24 & 0xFF) * paramFloat));
-        return (m & 0xFF) << 24 | (i & 0xFF) << 16 | (j & 0xFF) << 8 | k & 0xFF;
+    public static Color getGradientOffset(Color color1, Color color2, double offset) {
+        if (offset > 1) {
+            double left = offset % 1;
+            int off = (int) offset;
+            offset = off % 2 == 0 ? left : 1 - left;
+        }
+        double inverse_percent = 1 - offset;
+        int redPart = (int) (color1.getRed() * inverse_percent + color2.getRed() * offset);
+        int greenPart = (int) (color1.getGreen() * inverse_percent + color2.getGreen() * offset);
+        int bluePart = (int) (color1.getBlue() * inverse_percent + color2.getBlue() * offset);
+        return new Color(redPart, greenPart, bluePart);
+    }
+
+    public static int astolfoColors(int yOffset, int yTotal) {
+        float speed = 2900F;
+        float hue = (float) (System.currentTimeMillis() % (int)speed) + ((yTotal - yOffset) * 7);
+        while (hue > speed) {
+            hue -= speed;
+        }
+        hue /= speed;
+        if (hue > 0.5) {
+            hue = 0.5F - (hue - 0.5f);
+        }
+        hue += 0.5F;
+        return Color.HSBtoRGB(hue, 0.64F, 1F);
+    }
+
+    public static int lemonColors(float seconds, long index) {
+        index += 40;
+        float hue = ((System.currentTimeMillis() + index) % (int)(seconds * 1000)) / (seconds * 1000);
+        float hue2 = hue * 2;
+        if(hue2 > 1) {
+            hue2 = 2 - hue2;
+        }
+        return getGradientOffset(new Color(0, 255, 13), new Color(255, 234, 1), hue2).getRGB();
+    }
+
+
+    public static int lemonGreenColors(float seconds, long index) {
+        index += 40;
+        float hue = ((System.currentTimeMillis() + index) % (int)(seconds * 1000)) / (seconds * 1000);
+        float hue2 = hue * 2;
+        if(hue2 > 1) {
+            hue2 = 2 - hue2;
+        }
+        return getGradientOffset(new Color(144, 255, 48, 190), new Color(19, 128, 22), hue2).getRGB();
+    }
+
+    public static int lemonColors2(float seconds, long index) {
+        index += 40;
+        float hue = ((System.currentTimeMillis() + index) % (int)(seconds * 1000)) / (seconds * 1000);
+        float hue2 = hue * 2;
+        if(hue2 > 1) {
+            hue2 = 2 - hue2;
+        }
+        return getGradientOffset(new Color(144, 253, 106), new Color(255, 215, 110), hue2).getRGB();
+    }
+
+    public static int customColors(Color c1, Color c2, float seconds, long index) {
+        index += 40;
+        float hue = ((System.currentTimeMillis() + index) % (int)(seconds * 1000)) / (seconds * 1000);
+        float hue2 = hue * 2;
+        if(hue2 > 1) {
+            hue2 = 2 - hue2;
+        }
+
+        return getGradientOffset(c1, c2, hue2).getRGB();
     }
 
 }
