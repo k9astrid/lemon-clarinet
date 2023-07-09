55dbc95d04acf19759fa628dbd04af7513a26ea5
Eternadox <77886012+eternadox@users.noreply.github.com>
2023-07-09 19:09:06 -0400

    clickgui dragging and rectangle

---
+++ src/minecraft/dev/lemon/recode/module/impl/render/ClickGUI.java
@@ -0,0 +1,19 @@
+package dev.lemon.recode.module.impl.render;
+
+
+import dev.lemon.recode.gui.clickgui.ClickGui;
+import dev.lemon.recode.module.Category;
+import dev.lemon.recode.module.Module;
+import dev.lemon.recode.module.ModuleInfo;
+
+import org.lwjgl.input.Keyboard;
+
+@ModuleInfo(name = "ClickGUI", key = Keyboard.KEY_RSHIFT, category = Category.RENDER, suffix = "")
+public class ClickGUI extends Module {
+
+
+    @Override
+    public void onEnable(){
+        mc.displayGuiScreen(new ClickGui());
+    }
+}
