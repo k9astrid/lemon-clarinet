93e60000dcceeca6eea821be23464552cc911fc0
Eternadox <77886012+eternadox@users.noreply.github.com>
2023-07-13 15:20:02 -0400

    removed some utils, recoded one
    bruh

--- src/minecraft/dev/lemon/recode/event/impl/EventHitSlowdown.java
+++
@@ -1,8 +0,0 @@
-package dev.lemon.recode.event.impl;
-
-import best.azura.eventbus.events.CancellableEvent;
-
-public final class EventHitSlowdown extends CancellableEvent {
-    public double slowDown;
-    public boolean sprint;
-}
--- src/minecraft/dev/lemon/recode/utils/math/TimerUtil.java
+++ src/minecraft/dev/lemon/recode/utils/math/TimerUtil.java
@@ -1,47 +1,27 @@
 package dev.lemon.recode.utils.math;
 
-public class TimerUtil {
-	private long lastTime = System.currentTimeMillis();
-	private long lastMS = 0L;
 
-	public boolean hasReached(long time) {
-		if(System.currentTimeMillis() - lastTime >= time) {
-			return true;
-		}
-		return false;
+public class TimerUtil {
+	private long lastMillis;
+
+
+	public TimerUtil(){
+		this.reset();
 	}
-	
-	public long getTimeElapsed() {
-		return System.currentTimeMillis() - lastTime;
+
+	public void reset(){
+		this.lastMillis = System.currentTimeMillis();
 	}
-	public void reset() {
-		this.lastMS = this.getCurrentMS();
+
+	public long getElapsedTime(){
+		return System.currentTimeMillis() - this.lastMillis;
 	}
-	public long getCurrentMS() {
-		return System.currentTimeMillis();
+
+	public boolean hasTimeElapsed(long millis){
+		return getElapsedTime() >= millis;
 	}
-	public int convertToMS(int perSecond) {
-		return 1000 / perSecond;
-	}
-	public void setCurrentDifference(int difference) {
-		this.lastMS = System.currentTimeMillis() - (long)difference;
-	}
-	public boolean hasTimePassed(long delay) {
-		return System.currentTimeMillis() >= this.lastMS + delay;
-	}
-	public void setLastMS() {
-		this.lastMS = System.currentTimeMillis();
-	}
-	public boolean hasTimeElapsed(long time, boolean reset) {
-		if (this.lastMS > System.currentTimeMillis()) {
-			this.lastMS = System.currentTimeMillis();
-		}
-		if (System.currentTimeMillis() - this.lastMS > time) {
-			if (reset) {
-				this.reset();
-			}
-			return true;
-		}
-		return false;
+
+	public long getLastMillis(){
+		return lastMillis;
 	}
 }
