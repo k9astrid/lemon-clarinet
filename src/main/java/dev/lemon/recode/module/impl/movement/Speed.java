1300559aea2ec9f24f2ab9868ebf7ed9c69b9493
Eternadox <77886012+eternadox@users.noreply.github.com>
2023-07-14 15:17:15 -0400

    added test speed

--- src/main/java/dev/lemon/recode/module/impl/movement/Speed.java
+++ src/main/java/dev/lemon/recode/module/impl/movement/Speed.java
@@ -6,22 +6,44 @@ import dev.lemon.recode.event.impl.EventPreMotion;
 import dev.lemon.recode.module.Category;
 import dev.lemon.recode.module.Module;
 import dev.lemon.recode.module.ModuleInfo;
+import dev.lemon.recode.setting.impl.ModeSetting;
 import dev.lemon.recode.utils.player.MoveUtil;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.potion.Potion;
+import net.minecraft.util.MathHelper;
 import org.lwjgl.input.Keyboard;
 
-@ModuleInfo(name = "Speed", key = Keyboard.KEY_F, category = Category.MOVEMENT, suffix = "Strafe")
+@ModuleInfo(name = "Speed", key = Keyboard.KEY_F, category = Category.MOVEMENT, suffix = "")
 public class Speed extends Module {
+    private ModeSetting mode = new ModeSetting("Mode", "Test", "Strafe", "Test");
     @Override
-    public void onEnable(){
-        super.onEnable();
+    public void onDisable(){
+        super.onDisable();
+        mc.timer.timerSpeed = 1.0f;
+
     }
 
     @EventHandler
     public Listener<EventPreMotion> eventPreMotionListener = e -> {
-        if(mc.thePlayer.onGround){
-            mc.thePlayer.jump();
+        this.setSuffix(mode.getMode());
+        switch (mode.getMode()) {
+            case "Strafe":
+                if (mc.thePlayer.onGround) {
+                    mc.thePlayer.jump();
+                }
+                MoveUtil.strafe();
+                break;
+            case "Test":
+                if (mc.thePlayer.onGround){
+                    mc.timer.timerSpeed = 1.5f;
+                    mc.thePlayer.jump();
+                    mc.thePlayer.motionY *= 0.8;
+
+                } else {
+                    mc.timer.timerSpeed = 1.15f;
+                }
+
+                break;
         }
-        MoveUtil.strafe();
     };
 }
