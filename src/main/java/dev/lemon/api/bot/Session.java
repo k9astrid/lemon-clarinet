7034774e0324f60d0f5373d1dd4167de4d9b2dd5
Szypko <108012640+Szypko@users.noreply.github.com>
2023-07-19 17:04:43 +0200

    fix for now...

--- src/main/java/dev/lemon/api/bot/Session.java
+++ src/main/java/dev/lemon/api/bot/Session.java
@@ -1,4 +1,19 @@
 package dev.lemon.api.bot;
 
+import dev.lemon.api.bot.proxy.Proxy;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Session {
+    public String username;
+
+    public Proxy proxy;
+
+    public void join() {
+        (new Thread(() -> {
+            try {
+
+            } catch (Exception ignored) { }
+        })).start();
+    }
 }
