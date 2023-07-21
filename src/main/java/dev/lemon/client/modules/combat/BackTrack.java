f1f75f2c1b61fd0fbc16d293bdfe4bc94fe99aae
Eternadox <77886012+eternadox@users.noreply.github.com>
2023-07-21 17:17:16 -0400

    added backtrack back (its not that bad)

--- src/main/java/dev/lemon/client/modules/combat/BackTrack.java
+++ src/main/java/dev/lemon/client/modules/combat/BackTrack.java
@@ -1,5 +1,5 @@
 package dev.lemon.client.modules.combat;
-/*
+
 import dev.lemon.api.event.IEventListener;
 import dev.lemon.api.event.annotations.Subscribe;
 import dev.lemon.api.module.Module;
@@ -298,4 +298,3 @@ public class BackTrack extends Module {
             }
     }
 }
-*/
