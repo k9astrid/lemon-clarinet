29e48b395949e1de5c15a0079e1e7bde6e6f6010
Szypko <108012640+Szypko@users.noreply.github.com>
2023-07-22 15:21:21 +0200

    Sense watermark

--- src/main/java/dev/lemon/client/modules/render/HUD.java
+++ src/main/java/dev/lemon/client/modules/render/HUD.java
@@ -17,6 +17,7 @@ import dev.lemon.api.event.IEventListener;
 import dev.lemon.api.utils.player.MoveUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.util.EnumChatFormatting;
 
 import javax.vecmath.Vector2d;
 import java.awt.*;
@@ -27,10 +28,13 @@ import java.util.Comparator;
 public class HUD extends Module {
 
     //If u have a better way to do it then go ahead im stupid sorry
-    public ModeSetting watermark = new ModeSetting("Watermark", "Basic", "Basic", "Neverlose");
+    public ModeSetting watermark = new ModeSetting("Watermark", "Basic",
+            "Basic", "Neverlose", "Sense");
+
     public ModeSetting color = new ModeSetting("Color", "Venomous", "Venomous", "Peachy", "Sand Dune",
             "Orange Coral", "Plum Plate", "Toxic", "Orbital", "Celestial", "Mirror", "Rock", "Eternal Constance",
             "Exotic", "Antarctica", "Piglet");
+
     public NumberSetting offset = new NumberSetting("Offset", 3, 0, 10, 0.1);
     public static BooleanSetting toggleNotifications = new BooleanSetting("Toggle Notifications", false);
     public static BooleanSetting optimizeVisuals = new BooleanSetting("Optimize Visuals", false);
@@ -51,13 +55,37 @@ public class HUD extends Module {
 
     @Subscribe
     public final IEventListener<Render2DEvent> onRender2D = e -> {
+        float x, y;
+
         switch (watermark.getMode()) {
             case "Basic":
                 drawBasic();
                 break;
 
+            case "Sense":
+                String text = EnumChatFormatting.WHITE + "lemon" + EnumChatFormatting.RESET + "sense" + EnumChatFormatting.WHITE +
+                        " - " + mc.player.getName() + " - " + (mc.isSingleplayer() ? "singleplayer" : mc.getCurrentServerData().serverIP) + " ";
+
+                x = 4.5f;
+                y = 4.5f;
+
+                Gui.drawRect2(x, y, Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 7, 18.5, new Color(59, 57, 57).getRGB());
+                Gui.drawRect2(x + 2.5, y + 2.5, Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 2, 13, new Color(23, 23, 23).getRGB());
+
+                Gui.drawRect2(x + 1, y + 1, Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 5, .5, new Color(59, 57, 57).darker().getRGB());
+                Gui.drawRect2(x + 1, y + 17, Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 5, .5, new Color(59, 57, 57).darker().getRGB());
+                Gui.drawRect2(x + 1.5 + Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 4, y + 1.5, .5, 16, new Color(59, 57, 57).darker().getRGB());
+                Gui.drawRect2(x + 1, y + 1.5, .5, 16, new Color(59, 57, 57).darker().getRGB());
+
+                RenderUtil.drawGradientLR((float) (x + 2.5), (float) (y + 14.5), Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 2, 1, 1, Lemon.INSTANCE.getColorManager().getColor().getFirstColor(), Lemon.INSTANCE.getColorManager().getColor().getSecondColor());
+                Gui.drawRect2(x + 2.5, y + 16, Fonts.GREYCLIFF_BOLD_16.getStringWidth(text) + 2, .5, new Color(59, 57, 57).darker().getRGB());
+
+                Fonts.GREYCLIFF_BOLD_16.drawString(text, x + 4.5f, y + 5.5f, Lemon.INSTANCE.getColorManager().getColor().getFirstColor().getRGB());
+                break;
+
             case "Neverlose":
-                float x = 3, y = 4;
+                x = 3;
+                y = 4;
 
                 final String name = "LEMON",
                         ip = (mc.getCurrentServerData() == null ? "Singleplayer" : mc.getCurrentServerData().serverIP),
