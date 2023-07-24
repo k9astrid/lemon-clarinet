8fe33d1bd641d9d52bb264765a142789fc5352df
Szypko <108012640+Szypko@users.noreply.github.com>
2023-07-24 19:54:24 +0200

    fix?

--- src/main/java/dev/lemon/client/modules/movement/Speed.java
+++ src/main/java/dev/lemon/client/modules/movement/Speed.java
@@ -75,7 +75,7 @@ public class Speed extends Module {
                         if (KillAura.target != null) {
                             y = 0;
                             if (mc.player.onGround)
-                                MoveUtil.strafe((MoveUtil.baseSpeed() * 0.65f));
+                                MoveUtil.strafe((MoveUtil.baseSpeed() * 0.63f));
                             return;
                         }
 
