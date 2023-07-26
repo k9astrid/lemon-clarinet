019c10f568840d19dfc4045e69e6d4bc8c96b045
Szypko <108012640+Szypko@users.noreply.github.com>
2023-07-26 21:57:14 +0200

    fix.

--- src/main/java/net/minecraft/util/MovementInputFromOptions.java
+++ src/main/java/net/minecraft/util/MovementInputFromOptions.java
@@ -1,5 +1,7 @@
 package net.minecraft.util;
 
+import dev.lemon.client.events.input.MoveInputEvent;
+import dev.lemon.client.main.Lemon;
 import net.minecraft.client.settings.GameSettings;
 
 public class MovementInputFromOptions extends MovementInput
@@ -39,10 +41,18 @@ public class MovementInputFromOptions extends MovementInput
         this.jump = this.gameSettings.keyBindJump.isKeyDown();
         this.sneak = this.gameSettings.keyBindSneak.isKeyDown();
 
+        final MoveInputEvent moveInputEvent = new MoveInputEvent(moveForward, moveStrafe, 0.3f, jump, sneak);
+        Lemon.INSTANCE.getEventBus().handle(moveInputEvent);
+
+        this.moveForward = moveInputEvent.getForward();
+        this.moveStrafe = moveInputEvent.getStrafe();
+        this.jump = moveInputEvent.isJump();
+        this.sneak = moveInputEvent.isSneak();
+
         if (this.sneak)
         {
-            this.moveStrafe = (float)((double)this.moveStrafe * 0.3D);
-            this.moveForward = (float)((double)this.moveForward * 0.3D);
+            this.moveStrafe = (float)((double)this.moveStrafe * moveInputEvent.getSneakSpeed());
+            this.moveForward = (float)((double)this.moveForward * moveInputEvent.getSneakSpeed());
         }
     }
 }
